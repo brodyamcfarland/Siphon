c3fb59b5ee5ac7369f26bc021a1f29bb1d4cd457
Brody McFarland <brodyamcfarland@gmail.com>
2023-05-16 11:54:14 -0700

    Added Layout and some instructions

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import { FaGasPump } from "react-icons/fa";
+
+const Header = () => {
+     return (
+          <header className="flex flex-col items-center justify-center pt-2">
+               <div className="flex gap-2 items-center justify-center">
+                    <h1 className="tracking-widest text-2xl">SIPHON</h1>
+                    <FaGasPump size={25} className="text-emerald-200" />
+               </div>
+               <h2 className="text-xs text-gray-300">Free HTML Web Scraper</h2>
+          </header>
+     );
+};
+
+export default Header;
