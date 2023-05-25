d2153c1f0c34c7833f2e6864d761eadc0f0230c9
Brody McFarland <brodyamcfarland@gmail.com>
2023-05-25 15:10:58 -0700

    Scraper now works as intended. Working on some tweaks and copying to clipboard functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,19 +3,21 @@
 import { useState } from "react";
 import ScrapeForm from "@/components/ScrapeForm";
 import About from "@/components/About";
-
-// !    TODO
-// Work on logic for the web scrape function and put it in its own component
+import { Toaster } from "react-hot-toast";
 
 export default function Home() {
      const [showScrape, setShowScrape] = useState<boolean>(false);
+
      return (
           <>
                {showScrape ? (
-                    <ScrapeForm setShowScrape={setShowScrape} />
+                    <>
+                         <ScrapeForm setShowScrape={setShowScrape} />
+                    </>
                ) : (
                     <About setShowScrape={setShowScrape} />
                )}
+               <Toaster />
           </>
      );
 }
--- src/components/ScrapeForm.tsx
+++ src/components/ScrapeForm.tsx
@@ -6,12 +6,14 @@ import {
      TbCircleNumber3,
      TbCircleNumber4,
 } from "react-icons/tb";
+import { FiCopy } from "react-icons/fi";
+import { PuffLoader } from "react-spinners";
 import { TbShovel } from "react-icons/tb";
 import { BsArrowReturnLeft } from "react-icons/bs";
 import { htmlTagOptions } from "@/HTML_Options";
 import { Dispatch, SetStateAction, useState } from "react";
-import axios from "axios";
 import cheerio from "cheerio";
+import toast from "react-hot-toast";
 
 interface Props {
      setShowScrape: Dispatch<SetStateAction<boolean>>;
@@ -22,48 +24,64 @@ const ScrapeForm = ({ setShowScrape }: Props) => {
      const [htmlTag, setHtmlTag] = useState<string>("");
      const [htmlClass, setHtmlClass] = useState<string>("");
      const [htmlId, setHtmlId] = useState<string>("");
+     const [showHtmlResults, setShowHtmlResults] = useState<boolean>(false);
+     const [htmlResults, setHtmlResults] = useState<string>("");
      const [error, setError] = useState<string>("");
      const [loading, setLoading] = useState<boolean>(false);
 
      const scrape = async () => {
-          if (url !== "") {
-               try {
-                    setLoading(true);
-                    setError("");
+          if (url === "" || !url.includes("https://")) {
+               setError("URL");
+               toast.error("The URL is not valid.");
+               return;
+          }
+          if (htmlTag === "") {
+               setError("TAG");
+               toast.error("Please choose an HTML Tag.");
+               return;
+          }
+          try {
+               setLoading(true);
+               setError("");
 
-                    const response = await fetch(
-                         `/api/scrape/${encodeURIComponent(url)}`
-                    );
-                    const html = response.text();
-                    console.log(await html);
-                    const $ = cheerio.load(await html);
+               const response = await fetch(
+                    `/api/scrape/${encodeURIComponent(url)}`
+               );
+               const html = response.text();
+               console.log(await html);
+               const $ = cheerio.load(await html);
 
-                    // Use cheerio selectors to extract desired data from the response
-                    let scrapedData;
+               // Use cheerio selectors to extract desired data from the response
+               let scrapedData;
 
-                    if (htmlClass !== "") {
-                         scrapedData = $(htmlTag + "." + htmlClass).text();
-                    } else if (htmlId !== "") {
-                         scrapedData = $("#" + htmlId).text();
-                    } else {
-                         scrapedData = $(htmlTag).text();
-                    }
-
-                    console.log(scrapedData);
-               } catch (error) {
-                    setError("An error occurred during scraping.");
-                    console.log(error);
-               } finally {
-                    setLoading(false);
+               if (htmlClass !== "") {
+                    scrapedData = $(htmlTag + "." + htmlClass).text();
+               } else if (htmlId !== "") {
+                    scrapedData = $("#" + htmlId).text();
+               } else {
+                    scrapedData = $(htmlTag).text();
                }
+               console.log(scrapedData);
+               const tagsArray = scrapedData.split(/(?=[A-Z])/);
+               const formattedData = tagsArray.join("\n");
+               setHtmlResults(formattedData);
+          } catch (error) {
+               setError("SCRAPEFAIL");
+               toast.error("Something went wrong during scrape");
+               console.log(error);
+          } finally {
+               setLoading(false);
+               setShowHtmlResults(true);
+               setError("");
+               toast.success("Scrape Successful!");
           }
      };
 
      return (
-          <main className="flex flex-col items-center mt-10 md:mt-20 max-w-xl md:mx-auto text-xs md:text-sm mx-2 gap-4 ">
+          <main className="flex flex-col items-center mt-10 md:mt-20 max-w-xl md:mx-auto text-xs md:text-sm mx-2">
                <button
                     onClick={() => setShowScrape(false)}
-                    className="p-1 px-2 rounded-md text-white  opacity-80 hover:opacity-100 duration-300 text-xs border-2 border-emerald-500"
+                    className="p-1 px-2 rounded-md text-white  opacity-80 hover:opacity-100 duration-300 text-xs border-2 border-emerald-500 mb-4"
                >
                     <div className="flex gap-2 items-center justify-center">
                          <span>Return to Instructions</span>
@@ -81,16 +99,20 @@ const ScrapeForm = ({ setShowScrape }: Props) => {
                                              className="text-emerald-600 shrink-0"
                                         />
                                         <p>
-                                             Website Url
-                                             <span className="text-xs pl-2 text-gray-400">
-                                                  (https://www.xyz.com/products)
+                                             Website Url{" "}
+                                             <span className="text-orange-500 font-bold text-xs">
+                                                  (Required)
                                              </span>
                                         </p>
                                    </div>
                                    <input
                                         type="text"
-                                        className="ml-8 pl-2 rounded-md text-black focus:bg-emerald-200"
+                                        className={`ml-8 pl-2 rounded-md text-black focus:bg-emerald-200 ${
+                                             error === "URL" &&
+                                             "ring-2 ring-red-500 bg-red-300"
+                                        }`}
                                         onChange={(e) => setUrl(e.target.value)}
+                                        placeholder="https://www.xyz.com/products"
                                    />
                               </div>
                               <div className="flex flex-col ">
@@ -100,10 +122,16 @@ const ScrapeForm = ({ setShowScrape }: Props) => {
                                              className="text-emerald-600 shrink-0"
                                         />
                                         <p>HTML Tag</p>
+                                        <span className="text-orange-500 font-bold text-xs">
+                                             (Required)
+                                        </span>
                                    </div>
                                    <select
                                         value={htmlTag}
-                                        className="ml-8 pl-2 rounded-md text-black focus:bg-emerald-200"
+                                        className={`ml-8 pl-2 rounded-md text-black focus:bg-emerald-200 ${
+                                             error === "TAG" &&
+                                             "ring-2 ring-red-500 bg-red-300"
+                                        }`}
                                         onChange={(e) =>
                                              setHtmlTag(e.target.value)
                                         }
@@ -125,7 +153,7 @@ const ScrapeForm = ({ setShowScrape }: Props) => {
                                         <p>
                                              Class
                                              <span className="text-xs pl-2 text-gray-400">
-                                                  (ex. "product_price")
+                                                  (Optional)
                                              </span>
                                         </p>
                                    </div>
@@ -135,6 +163,7 @@ const ScrapeForm = ({ setShowScrape }: Props) => {
                                         onChange={(e) =>
                                              setHtmlClass(e.target.value)
                                         }
+                                        placeholder="product_price"
                                    />
                               </div>
                               <div className="flex flex-col">
@@ -146,7 +175,7 @@ const ScrapeForm = ({ setShowScrape }: Props) => {
                                         <p>
                                              Id
                                              <span className="text-xs pl-2 text-gray-400 ">
-                                                  (ex. "unique_identifier")
+                                                  (optional)
                                              </span>
                                         </p>
                                    </div>
@@ -156,14 +185,65 @@ const ScrapeForm = ({ setShowScrape }: Props) => {
                                         onChange={(e) =>
                                              setHtmlId(e.target.value)
                                         }
+                                        placeholder="unique_identifier"
                                    />
                               </div>
                          </div>
                     </form>
                </div>
+               {showHtmlResults && (
+                    <div className="max-w-lg w-full bg-[#0d140c] text-white flex flex-col p-4 border-r border-l border-b border-white/20 rounded-b-md mx-2 md:mx-0">
+                         <span className="text-center">
+                              Showing HTML Results for:
+                         </span>
+                         <span className="text-xs text-center text-gray-400">
+                              Tag Name: {`<${htmlTag}>`}
+                         </span>
+                         {htmlClass !== "" && (
+                              <span className="text-xs text-center text-gray-400">
+                                   Class Name: {htmlClass}
+                              </span>
+                         )}
+                         {htmlId !== "" && (
+                              <span className="text-xs text-center text-gray-400">
+                                   Id Name: {htmlId}
+                              </span>
+                         )}
+                         <div className="relative text-xs p-3 pb-10 rounded-md border border-white/20 bg-black mt-4">
+                              {loading ? (
+                                   <div className="flex flex-col items-center justify-center">
+                                        <PuffLoader
+                                             color="white"
+                                             size={30}
+                                             speedMultiplier={0.9}
+                                        />
+                                        <span className="uppercase text-[8px] tracking-widest animate-pulse">
+                                             Loading
+                                        </span>
+                                   </div>
+                              ) : (
+                                   <p className="overflow-y-auto max-h-[13rem] whitespace-pre-wrap">
+                                        {htmlResults.length === 0 ? (
+                                             <span className="text-center flex items-center justify-center uppercase text-[10px] tracking-widest ">
+                                                  No Results found
+                                             </span>
+                                        ) : (
+                                             <>{htmlResults}</>
+                                        )}
+                                   </p>
+                              )}
+
+                              <button className="truncate overflow-x-clip absolute bottom-0 right-0 rounded-br-md p-1 border-white/20 flex gap-2 items-center justify-end border hover:border-white hover:bg-white/20 duration-300">
+                                   <span>Copy</span>
+                                   <FiCopy />
+                              </button>
+                         </div>
+                    </div>
+               )}
                <button
                     onClick={() => scrape()}
-                    className="my-4 py-2 px-10 rounded-md text-white bg-gradient-to-br from-emerald-800 to-green-900 opacity-80 hover:opacity-100 duration-300 border border-white/20"
+                    className="my-4 py-2 px-10 rounded-md text-white bg-gradient-to-br from-emerald-800 to-green-900 opacity-80 hover:opacity-100 duration-300 border border-white/20 disabled:opacity-30"
+                    disabled={!url || !htmlTag}
                >
                     <div className="flex gap-2 items-center justify-center text-sm ">
                          <span>Scrape</span>
